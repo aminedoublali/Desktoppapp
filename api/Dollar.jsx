702d45a083b444840796bd0010abb88bc550eacd
Aminesam1092 <doublaliamin13@gmail.com>
2023-07-10 23:47:18 +0900

    Update :　記述の追加

--- api/Dollar.jsx
+++ api/Dollar.jsx
@@ -1,32 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import CHF from "../components/CHF";
+import EUR from "../components/EU";
+import GBP from "../components/GBP";
 import JP from "../components/JP";
+import MAD from "../components/MAD";
+import SGD from "../components/SGD";
 export default function Dollar() {
-    const [rates, setRates] = useState({});
+  const [rates, setRates] = useState({});
 
-    useEffect(() => {
-        fetch('https://api.exchangerate-api.com/v4/latest/USD')
-            .then(response => response.json())
-            .then(data => {
-                setRates({
-                    JPY: data.rates['JPY'],
-                    GBP: data.rates['GBP'],
-                    CHF: data.rates['CHF'],
-                    SGD: data.rates['SGD'],
-                });
-            });
-    }, []);
+  useEffect(() => {
+    fetch("https://api.exchangerate-api.com/v4/latest/USD")
+      .then((response) => response.json())
+      .then((data) => {
+        setRates({
+          JPY: data.rates["JPY"],
+          GBP: data.rates["GBP"],
+          CHF: data.rates["CHF"],
+          SGD: data.rates["SGD"],
+          EUR: data.rates["EUR"],
+          MAD: data.rates["MAD"],
+        });
+      });
+  }, []);
 
-    return (
-        <div className='flex items-center'>
-            <div className='flex items-center'>
-                {rates.JPY ? ` ${rates.JPY}` : 'Loading...'}
-                <JP />
-            </div>
-            <div>
-                {rates.CHF ? ` ${rates.CHF}` : 'Loading...'}
-                <CHF />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center ">
+    <div className="justify-between">
+      <div className="flex items-center justify-center font-sans text-xl font-semibold  p-4 m-4 color ml-4 box ">
+        <JP className='w-10 h-10'/>
+        {rates.JPY ? ` ${rates.JPY}` : "Loading..."}
+      </div>
+      <div  className="flex items-center justify-center font-sans text-xl font-semibold  p-4 m-4 color ml-4 box">
+        <EUR />
+        {rates.EUR ? ` ${rates.EUR}` : "Loading..."}
+      </div>
+      <div  className="flex items-center justify-center font-sans text-xl font-semibold  p-4 m-4 color ml-4 box">
+        <GBP />
+        {rates.GBP ? ` ${rates.GBP}` : "Loading..."}
+      </div>
+    </div>
+      <div className="justify-between">
+      <div className="flex items-center justify-center font-sans text-xl font-semibold  p-4 m-4 color ml-4 box">
+        <SGD className='w-10 h-10'/>
+        {rates.SGD ? ` ${rates.SGD}` : "Loading..."}
+      </div>
+      <div  className="flex items-center justify-center font-sans text-xl font-semibold  p-4 m-4 color ml-4 box">
+        <MAD />
+        {rates.MAD ? ` ${rates.MAD}` : "Loading..."}
+      </div>
+      <div  className="flex items-center justify-center font-sans text-xl font-semibold  p-4 m-4 color ml-4 box">
+        <CHF />
+        {rates.CHF ? ` ${rates.CHF}` : "Loading..."}
+      </div>
+    </div>
+    </div>
+  );
 }
--- pages/Dtailus.jsx
+++ pages/Dtailus.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react";
 import Dollar from "../api/Dollar";
-import MainLayout from '../components/Layouts/Mainlayout';
+import Button from "../components/Button";
+import MainLayout from "../components/Layouts/Mainlayout";
 import US from "../components/US";
 export default function Dtailus() {
   return (
-
     <MainLayout>
-        <div className=" bg-slate-200 shadow-md p-4 m-4 rounded-md">
-        <US />
-        <Dollar />
+      <div className="bg-slate-200 shadow-md p-4 m-4 rounded-md">
+        <div className="flex items-center">
+          <US />
+          <h1 className="font-sans text-xl font-semibold text-gray-600 ml-4 mr-2">
+            米ドルレート
+          </h1>
+          <Button className="w-24 bg-blue-500 text-white px-4 py-2  p-4 m-4 rounded-md hover:bg-blue-600">
+            売り
+          </Button>
+          <Button className="w-24 bg-red-400 text-white px-4 py-2  p-4 m-4 rounded-md hover:bg-red-600">
+            買い
+          </Button>
         </div>
- </MainLayout>
-  )
+        <Dollar />
+      </div>
+    </MainLayout>
+  );
 }
